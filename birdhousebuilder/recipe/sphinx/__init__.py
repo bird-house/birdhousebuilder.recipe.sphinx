6ec82c68c1e6ae1e4dc2a35e4bf497b3e0af47fc
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-10-22 15:45:22 +0200

    inital recipe

---
+++ birdhousebuilder/recipe/sphinx/__init__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Recipe sphinx
+
+Based on https://pypi.python.org/pypi/collective.recipe.sphinxbuilder
+"""
+
+import logging
+import os
+import re
+import sys
+#import zc.buildout
+#from fnmatch import fnmatch
+from birdhousebuilder.recipe import conda
+
+log = logging.getLogger(__name__)
+
+
+class Recipe(object):
+    """buildout recipe to setup sphinx for birdhouse components"""
+
+    def __init__(self, buildout, name, options):
+        self.buildout, self.name, self.options = buildout, name, options
+
+        self.outputs = options.get('outputs', 'html')
+
+        self.build_dir = os.path.join(self.buildout_dir, options.get('build', 'docs'))
+        self.source_dir = options.get('source', os.path.join(self.build_dir, 'source'))
+
+    def install(self):
+        """Installer"""
+        installed = []
+        installed += list(self.install_conda())
+
+        return installed
+
+    def install_conda(self):
+        script = conda.Recipe(
+            self.buildout,
+            self.name,
+            {'pkgs': 'sphinx sphinx_rtd_theme'})
+        return script.install()
+
+    def update(self):
+        return self.install()
+
