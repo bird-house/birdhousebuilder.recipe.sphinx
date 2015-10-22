070489f15fe4261d94334f32dc05959adf1eb53a
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-10-22 18:01:28 +0200

    added makefile and conf.py template

--- birdhousebuilder/recipe/sphinx/__init__.py
+++ birdhousebuilder/recipe/sphinx/__init__.py
@@ -5,32 +5,43 @@ Recipe sphinx
 Based on https://pypi.python.org/pypi/collective.recipe.sphinxbuilder
 """
 
-import logging
 import os
 import re
 import sys
-#import zc.buildout
+from mako.template import Template
+import zc.buildout
 #from fnmatch import fnmatch
 from birdhousebuilder.recipe import conda
 
+import logging
 log = logging.getLogger(__name__)
 
+makefile = Template(filename=os.path.join(os.path.dirname(__file__), "Makefile"))
+conf_py = Template(filename=os.path.join(os.path.dirname(__file__), "conf.py"))
 
 class Recipe(object):
     """buildout recipe to setup sphinx for birdhouse components"""
 
     def __init__(self, buildout, name, options):
         self.buildout, self.name, self.options = buildout, name, options
+        self.buildout_dir = self.buildout['buildout']['directory']
 
         self.outputs = options.get('outputs', 'html')
 
-        #self.build_dir = os.path.join(self.buildout_dir, options.get('build', 'docs'))
-        #self.source_dir = options.get('source', os.path.join(self.build_dir, 'source'))
+        self.docs_dir = os.path.join(self.buildout_dir, options.get('docs', 'docs'))
+        self.build_dir = os.path.join(self.docs_dir, 'build')
+        self.source_dir = os.path.join(self.docs_dir, 'source')
+
+        self.options['project'] = self.options.get('project', 'MyBird')
+        self.options['version'] = self.options.get('version', '0.1')
 
     def install(self):
         """Installer"""
         installed = []
         installed += list(self.install_conda())
+        installed += list(self.install_dir())
+        installed += list(self.install_makefile())
+        installed += list(self.install_config())
 
         return installed
 
@@ -41,6 +52,30 @@ class Recipe(object):
             {'pkgs': 'sphinx sphinx_rtd_theme mako docutils'})
         return script.install()
 
+    def install_dir(self):
+        # create build folder
+        conda.makedirs(self.build_dir)
+
+        # create source folder
+        conda.makedirs(self.source_dir)
+
+        return tuple()
+
+    def install_makefile(self):
+        content = makefile.render(**self.options)
+        name = os.path.join(self.docs_dir, 'Makefile')
+        self._write_file(name, content)
+        return [name]
+
+    def install_config(self):
+        content = conf_py.render(**self.options)
+        name = os.path.join(self.source_dir, 'conf.py')
+        self._write_file(name, content)
+        return [name]
+
     def update(self):
         return self.install()
 
+    def _write_file(self, name, content):
+        with open(name, 'w') as fp:
+            fp.write(content)
