a3c3db9317f1ae673a7792a8da8fb1cf5f3fd300
Carsten Ehbrecht <ehbrecht@dkrz.de>
2018-01-17 18:13:28 +0100

    version 0.2.4

--- setup.py
+++ setup.py
@@ -5,18 +5,20 @@ This module contains the tool of birdhousebuilder.recipe.sphinx
 import os
 from setuptools import setup, find_packages
 
+
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
+
 name = 'birdhousebuilder.recipe.sphinx'
 
-version = '0.2.3'
-description="Buildout recipe to generate and Sphinx-based documentation for Birdhouse."
+version = '0.2.4'
+description = "Buildout recipe to generate and Sphinx-based documentation for Birdhouse."
 long_description = (
     read('README.rst') + '\n' +
     read('AUTHORS.rst') + '\n' +
     read('CHANGES.rst')
-    )
+)
 
 entry_points = '''
 [zc.buildout]
@@ -29,7 +31,7 @@ reqs = ['setuptools',
         'zc.buildout',
         # -*- Extra requirements: -*-
         'mako',
-        'zc.recipe.egg', 
+        'zc.recipe.egg',
         'birdhousebuilder.recipe.conda',
         ],
 tests_reqs = ['zope.testing', 'zc.buildout']
@@ -40,24 +42,23 @@ setup(name=name,
       long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        'Framework :: Buildout',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 2',
-        'Topic :: Software Development :: Build Tools',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Scientific/Engineering :: Atmospheric Science',
-        ],
+          'Framework :: Buildout',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 2',
+          'Topic :: Software Development :: Build Tools',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Scientific/Engineering :: Atmospheric Science',
+      ],
       keywords='buildout sphinx birdhouse',
       author='Birdhouse',
       author_email='wps-dev at dkrz.de',
       url='https://github.com/birdhouse/birdhousebuilder.recipe.sphinx',
       license='Apache License 2.0',
-      install_requires = reqs,
-      extras_require = dict(tests=tests_reqs),
-      entry_points = entry_points,
-      packages = find_packages(exclude=['ez_setup']),
-      namespace_packages = ['birdhousebuilder', 'birdhousebuilder.recipe'],
-      include_package_data = True,
-      zip_safe = False,
+      install_requires=reqs,
+      extras_require=dict(tests=tests_reqs),
+      entry_points=entry_points,
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['birdhousebuilder', 'birdhousebuilder.recipe'],
+      include_package_data=True,
+      zip_safe=False,
       )
-
